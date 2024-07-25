33c8b83b04adf38cfb8cce2cef765fe2f355c8e2
Kate Kunnykova <kate@Kates-MacBook-Pro.local>
2024-07-24 21:58:26 -0400

    added tests for dropdowns

--- tests/dropdowns.spec.ts
+++ tests/dropdowns.spec.ts
@@ -32,25 +32,97 @@ test.describe('Dropdowns tests', () => {
         // expect(selectedOptions).toEqual(['California', 'Florida', 'Texas']);
     })
     
-    test('jQuery Dropdown Search', async ({ page }) => {
+    test('Select single value from dropdown with search', async ({ page }) => {
         await page.goto('https://www.lambdatest.com/selenium-playground/jquery-dropdown-search-demo');
-    
+        // Function to select an option
         async function selectOption(optionText: string) {
             const dropdown = page.locator('#country+span');
             await dropdown.click();
-            
             const searchInput = page.locator('span > input.select2-search__field');
             await searchInput.fill(optionText);
             const searchedOption = page.locator('span .select2-results > ul >li.select2-results__option');
             await searchedOption.click();
         }
-        
         const countries: string[] = ['India', 'Australia', 'United States of America'];
+        // Select countries
         for (let country of countries) {
             await selectOption(country);
             const selectedCountry = page.locator('span #select2-country-container');
             await expect(selectedCountry).toHaveText(country);
         }
+    })
+    test('Select multiple values by search', async ({ page }) => {
+        await page.goto('https://www.lambdatest.com/selenium-playground/jquery-dropdown-search-demo');
+        // Function to select an option
+        async function selectOption(optionText: string) {
+            const dropdown = page.locator('span.select2-selection--multiple');
+            await dropdown.click();
+            const searchInput = page.locator('input.select2-search__field');
+            await searchInput.fill(optionText);
+            const searchedOption = page.locator('li.select2-results__option');
+            await searchedOption.click();
+        }
+        const states: string[] = ['Alaska', 'California', 'Texas'];
+        // Select states
+        for (let state of states) {
+            await selectOption(state);
+            const selectedState = page.locator(`.select2-selection__choice[title=${state}]`);
+            await expect(selectedState).toBeVisible()
+        }
+        // Assert that the number of selected otions equals to the number of requested states
+        const selectedStateLength = await page.locator('li.select2-selection__choice').count();
+        expect(selectedStateLength).toEqual(states.length);
+    })
+    
+    test('Delete all options from the search', async ({ page }) => {
+        await page.goto('https://www.lambdatest.com/selenium-playground/jquery-dropdown-search-demo');
+        // Function to select an option
+        async function selectOption(optionText: string) {
+            const dropdown = page.locator('span.select2-selection--multiple');
+            await dropdown.click();
+            const searchInput = page.locator('input.select2-search__field');
+            await searchInput.fill(optionText);
+            const searchedOption = page.locator('li.select2-results__option');
+            await searchedOption.click();
+        }
+        // Function to delete a selected option
+        async function deleteOption(optionText:string) {
+            const deleteSelectedState = page.locator(`.select2-selection__choice[title=${optionText}] > .select2-selection__choice__remove`);
+            await deleteSelectedState.click();
+            const selectedState = page.locator(`.select2-selection__choice[title=${optionText}]`);
+            await expect(selectedState).not.toBeVisible();
+        }
+    
+        const states: string[] = ['Alaska', 'California', 'Texas'];
+        // Select all options
+        for (let state of states) {
+            await selectOption(state);
+            const selectedState = page.locator(`.select2-selection__choice[title=${state}]`);
+            await expect(selectedState).toBeVisible()
+        }
+        // Delete all selected options
+        for (let state of states) {
+            await deleteOption(state)
+        }
+        // Assert that all options are deleted
+        const selectInput = page.locator('ul.select2-selection__rendered');
+        await expect(selectInput).toBeEmpty();
+    })
+    
+    test('Disabled dropdown options remain unselectable', async ({ page }) => {
+        await page.goto('https://www.lambdatest.com/selenium-playground/jquery-dropdown-search-demo');
+        const dropdown = page.locator('select.js-example-disabled-results');
+        const disabledOption = dropdown.locator('option:disabled').first();
+        
+        await expect(disabledOption).toBeDisabled();
+        // Attempt to select the disabled option
+        const disabledValue = await disabledOption.getAttribute('value');
+        const titleOfDisabledOption = await disabledOption.textContent();
+        await dropdown.selectOption({ value: `${disabledValue}` });
+        const selectedValue = await page.locator('.select2-selection__rendered').nth(2).textContent();
+        // Bug: it is possible to select disabled value
+        // Ensure the disabled option is not selected
+        expect(selectedValue).not.toBe(titleOfDisabledOption);
     })
     
 })
