f55b984a98ecc0756cd6dd531d75c0b279014af0
Kate Kunnykova <48454133+KateL9@users.noreply.github.com>
2024-08-01 21:00:21 -0400

    Added more tests for iFrames

--- tests/frames.spec.ts
+++ tests/frames.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
-test.describe('Frames test', () => {
-    test('Interact with iframe', async ({ page }) => {
+test.describe('iFrames test', () => {
+    test('Interact with iFrame', async ({ page }) => {
         await page.goto('https://letcode.in/frame');
         const frame = page.frameLocator('iframe#firstFr');
         if (frame) {
@@ -14,7 +14,7 @@ test.describe('Frames test', () => {
         }
     })
 
-    test('Nested frame test', async ({ page }) => {
+    test('Nested iFrame test', async ({ page }) => {
         await page.goto('https://letcode.in/frame');
         const frame = page.frameLocator('iframe#firstFr');
         const innerFrame = frame.frameLocator('iframe[src="innerFrame"]');
@@ -27,5 +27,26 @@ test.describe('Frames test', () => {
             throw new Error('Frame not found');
         }
     })
+
+    test('Interact with iFrame and verify the response', async ({ page }) => {
+        await page.goto('https://codepen.io/getform/pen/jRoexL');
+        const iFrame = page.frameLocator('#result');
+        await iFrame.locator('input[name=email]').fill('test@email.com');
+        await iFrame.locator('input[name=name]').fill('John Smith');
+        await iFrame.locator('select[name=platform]').selectOption('Github');
+        await iFrame.locator('button[type="submit"]').click();
+
+        await expect(iFrame.getByText('We received your submission, thank you!')).toBeVisible()
+    })
+    
+    test('Test search within iFrames', async ({ page }) => {
+        await page.goto('https://www.w3schools.com/tags/tryit.asp?filename=tryhtml_iframe');
+        const outerIframe = page.frameLocator('iframe#iframeResult');
+        const iFrame = outerIframe.frameLocator('iframe[title="W3Schools Free Online Web Tutorials"]');
+        await iFrame.getByPlaceholder('Search our tutorials, e.g. HTML').fill('TypeScript');
+        await iFrame.locator('button#learntocode_searchbtn').click();
+        await expect(iFrame.getByRole('link', {name: 'Start learning TypeScript now »'})).toBeVisible();
+    })
+    
 })
 
