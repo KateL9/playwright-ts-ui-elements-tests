1b6ab3064dbae1d1921c0ca068be681955d44706
Kate Kunnykova <48454133+KateL9@users.noreply.github.com>
2024-08-03 12:38:15 -0400

    Added test for horizontal scrolling

--- tests/scroll.spec.ts
+++ tests/scroll.spec.ts
@@ -1,4 +1,4 @@
-import {test, expect} from '@playwright/test';
+import { test, expect } from '@playwright/test';
 
 test.describe('Testing scrolling', () => {
     test('Scroll to Contact Us', async ({ page }) => {
@@ -10,12 +10,21 @@ test.describe('Testing scrolling', () => {
 
     test('Verify lazy-loaded items render correctly upon scrolling', async ({ page }) => {
         await page.goto('https://ecommerce-playground.lambdatest.io');
-        await page.getByRole("button", {name:"Shop by Category"}).click();
-        await page.getByRole("link", {name: " Software"}).click();
+        await page.getByRole("button", { name: "Shop by Category" }).click();
+        await page.getByRole("link", { name: " Software" }).click();
         await page.mouse.wheel(0, 10000);
         const lastElement = await page.locator('div.product-layout').last();
         await expect(lastElement).toBeVisible();
     })
-    
-    
+
+    test('Horizontal scrolling test', async ({ page }) => {
+        await page.goto('https://codepen.io/ReGGae/full/QZxdVX/');
+        const iFrame = page.frameLocator('iframe#result');
+        for (let i = 1; i <= 10; i++) {
+            const randomIndex = Math.floor(Math.random() * 5) + 1;
+            const randomImage = iFrame.locator(`div:nth-child(3) > article:nth-child(${randomIndex}) > .slide__inner > .slide__img`);
+            randomImage.scrollIntoViewIfNeeded();
+            await expect(randomImage).toBeVisible();
+        }
+    })
 })
